13aa579037844bcde56cf5c0f69ed1828c54e1b9
Brendan Shelton <bpshelton01@gmail.com>
2019-08-07 20:09:03 -0500

    Added sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { ScoresComponent } from './scores/scores.component';
 
 const appRoutes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'scores', component: ScoresComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ScoresComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
       appRoutes
     ),
-    AppRoutingModule,
     MatGridListModule,
     MatMenuModule,
     MatButtonModule,
     MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatListModule,
     BrowserAnimationsModule
   ],
   providers: [],
